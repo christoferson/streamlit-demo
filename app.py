47eab0d2f05430995bdc09a944b223470220c170
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-03-13 00:55:03 +0900

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.write("Hello world")
 
@@ -9,3 +10,26 @@ df = pd.DataFrame({
 })
 
 df
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
