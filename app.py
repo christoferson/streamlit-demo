2963b55a1e11db134d7264ab610eee8282a540cb
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-05-06 00:36:40 +0900

    update requirement

--- app.py
+++ app.py
@@ -6,33 +6,32 @@ import jwt
 from typing import Any, Dict
 import json
 
-######
+###### Get Name from OIDC
 
-def _decode_access_token(access_token: str) -> Dict[str, Any]:
-    decoded = jwt.decode(access_token, options={"verify_signature": False})
-    return decoded
+#def _decode_access_token(access_token: str) -> Dict[str, Any]:
+#    decoded = jwt.decode(access_token, options={"verify_signature": False})
+#    return decoded
 
-def process_aws_oidc_token():
-   
-   identity = "unknown"
+def process_aws_oidc_data():
+
    access_token = "oidc_data"
    headers = _get_websocket_headers()
    if headers:
-      if "x-amzn-oidc-identity" in headers:
-         identity = headers["x-amzn-oidc-identity"]
-         #accesstoken = headers["x-amzn-oidc-accesstoken"]
-         #data = headers["x-amzn-oidc-data"]
-      if "X-Amzn-Oidc-Identity" in headers:
-         identity = headers["X-Amzn-Oidc-Identity"]
-
+      #if "X-Amzn-Oidc-Identity" in headers:
+      #   identity = headers["X-Amzn-Oidc-Identity"]
       if "X-Amzn-Oidc-Accesstoken" in headers:
-         access_token =  headers["X-Amzn-Oidc-Accesstoken"]
-         access_token_claim = _decode_access_token(access_token=access_token)
-         access_token = json.dumps(access_token_claim)
+         access_token_jwt =  headers["X-Amzn-Oidc-Accesstoken"]
+         #access_token_claim = _decode_access_token(access_token=access_token_jwt)
+         access_token_claims = jwt.decode(access_token_jwt, options={"verify_signature": False})
+         #access_token = json.dumps(access_token_claims)
+         if "name" in access_token_claims:
+            access_token = access_token_claims["name"]
+         elif "upn" in access_token_claims:
+            access_token = access_token_claims["upn"]
 
    return access_token
 
-session_identity = process_aws_oidc_token()
+session_identity = process_aws_oidc_data()
 st.write(f"Welcome: {session_identity}")
 
 
