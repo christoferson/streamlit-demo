5b33290c34236b3e06aa5776e869bbbcc31c7fd4
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-05-05 21:53:38 +0900

    Add process_oidc

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 import streamlit as st
 import cmn_auth
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+
+######
+
+def process_aws_oidc_token():
+   
+   identity = "unknown"
+   headers = _get_websocket_headers()
+   if headers:
+      if "x-amzn-oidc-identity" in headers:
+         identity = headers["x-amzn-oidc-identity"]
+         #accesstoken = headers["x-amzn-oidc-accesstoken"]
+         #data = headers["x-amzn-oidc-data"]
+      if "X-Amzn-Oidc-Identity" in headers:
+         identity = headers["X-Amzn-Oidc-Identity"]
+   
+   return identity
+
+session_identity = process_aws_oidc_token()
+st.write('Welcome: ', session_identity)
+
+
+####
+
 
 ###### AUTH START #####
 
