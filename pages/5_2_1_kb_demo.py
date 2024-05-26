1d5117e4fcc1dc06cb0b15bdb80ad3674dee1a77
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-05-27 00:07:45 +0900

    rename 5 2

--- pages/5_2_1_kb_demo.py
+++ pages/5_2_1_kb_demo.py
@@ -22,6 +22,19 @@ logging.basicConfig(level=logging.INFO)
 
 ######  AUTH END #####
 
+st.set_page_config(
+    page_title="Knowledge Base",
+    page_icon="🧊",
+    layout="centered", # "centered" or "wide"
+    initial_sidebar_state="expanded", #"auto", "expanded", or "collapsed"
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
+
+st.logo(icon_image="images/logo.png", image="images/logo_text.png")
 
 
 opt_model_id_list = [
@@ -35,9 +48,9 @@ print(opt_bedrock_kb_list)
 with st.sidebar:
     st.markdown(":blue[Settings]")
     opt_kb_id = st.selectbox(label="Knowledge Base ID", options=opt_bedrock_kb_list, index = 0, key="kb_id")
-    opt_kb_doc_count = st.slider(label="Document Count", min_value=1, max_value=24, value=3, step=1, key="kb_doc_count")
+    opt_kb_doc_count = st.slider(label="Document Count", min_value=1, max_value=24, value=5, step=1, key="kb_doc_count")
     opt_model_id = st.selectbox(label="Model ID", options=opt_model_id_list, index = 0, key="model_id")
-    opt_temperature = st.slider(label="Temperature", min_value=0.0, max_value=1.0, value=0.1, step=0.1, key="temperature")
+    opt_temperature = st.slider(label="Temperature", min_value=0.0, max_value=1.0, value=0.0, step=0.1, key="temperature")
     opt_top_p = st.slider(label="Top P", min_value=0.0, max_value=1.0, value=1.0, step=0.1, key="top_p")
     opt_top_k = st.slider(label="Top K", min_value=0, max_value=500, value=250, step=1, key="top_k")
     opt_max_tokens = st.slider(label="Max Tokens", min_value=0, max_value=4096, value=2048, step=1, key="max_tokens")
