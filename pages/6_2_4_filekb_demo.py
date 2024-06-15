6444c89d498234a1de37b33dd506956ce90e1369
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-06-16 01:38:16 +0900

    File Update

--- pages/6_2_4_filekb_demo.py
+++ pages/6_2_4_filekb_demo.py
@@ -5,8 +5,13 @@ import cmn_settings
 import logging
 from botocore.exceptions import ClientError
 
-#ValidationException: An error occurred (ValidationException) when calling the RetrieveAndGenerate operation: Token limit exceeded: 
-# input contains 26105 tokens and exceeds limit of 20000
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/retrieve_and_generate.html
+
+# ValidationException: An error occurred (ValidationException) when calling the RetrieveAndGenerate operation: Token limit exceeded: 
+# input contains 26105 tokens and exceeds limit of 
+
+# Error 2 - Haiku is not supported
+#The provided model is not supported for EXTERNAL_SOURCES RetrieveAndGenerateType. Update the model arn then retry your request.
 
 ###### AUTH START #####
 
@@ -20,14 +25,36 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 client = boto3.client("bedrock-agent-runtime", region_name=AWS_REGION)
 
-st.title("File Query v2 1")
+
+st.set_page_config(
+    page_title="File Query",
+    page_icon="📓",
+    layout="centered", # "centered" or "wide"
+    initial_sidebar_state="expanded", #"auto", "expanded", or "collapsed"
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
+
+st.logo(icon_image="images/logo.png", image="images/logo_text.png")
+
+opt_model_id_list = [
+    "anthropic.claude-3-sonnet-20240229-v1:0"
+]
+
+with st.sidebar:
+    st.markdown(":blue[Settings]")
+    opt_model_id = st.selectbox(label="Model ID", options=opt_model_id_list, index = 0, key="model_id")
+
+st.title("File Query v2.4")
 st.write("Knowledge Bases on File Upload for Amazon Bedrock")
 
+if "menu_filequery_messages" not in st.session_state:
+    st.session_state.menu_filequery_messages = []
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
+for message in st.session_state.menu_filequery_messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
         if "citations" in message:
@@ -35,9 +62,13 @@ for message in st.session_state.messages:
                 st.json(message["citations"])
 
 
+if "menu_filequery_session_id" in st.session_state:
+    menu_filequery_session_id = st.session_state["menu_filequery_session_id"]
+    st.markdown(f":blue[Session: {menu_filequery_session_id}]")
+
 #### 
 uploaded_file = st.file_uploader(
-    "The supported file types are PDF, MD, TXT, DOCX, HTML, CSV, XLS, and XLSX",
+    "Maximum of 20000 Tokens",
     type=["PDF", "MD", "TXT", "DOCX", "HTML", "CSV", "XLS", "XLSX"],
     accept_multiple_files=False,
 )
@@ -53,7 +84,7 @@ if prompt := st.chat_input(
     "Ask question about the file." if uploaded_file else "Choose and upload a file first",
     disabled=(not uploaded_file),
 ):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.menu_filequery_messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
@@ -63,11 +94,11 @@ if prompt := st.chat_input(
         try:
 
             params = {
-                "input": {"text": prompt},
+                "input": { "text": prompt },
                 "retrieveAndGenerateConfiguration": {
                     "type": "EXTERNAL_SOURCES",
                     "externalSourcesConfiguration": {
-                        "modelArn": "anthropic.claude-3-sonnet-20240229-v1:0",
+                        "modelArn": opt_model_id,
                         "sources": [
                             {
                                 "sourceType": "BYTE_CONTENT",
@@ -82,8 +113,8 @@ if prompt := st.chat_input(
                 },
             }
 
-            if "sessionId" in st.session_state:
-                params["sessionId"] = st.session_state["sessionId"]
+            if "menu_filequery_session_id" in st.session_state:
+                params["sessionId"] = st.session_state["menu_filequery_session_id"]
 
             response = client.retrieve_and_generate(**params)
 
@@ -91,8 +122,8 @@ if prompt := st.chat_input(
             with st.expander("citations"):
                 st.json(response["citations"])
 
-            st.session_state["sessionId"] = response["sessionId"]
-            st.session_state.messages.append(
+            st.session_state["menu_filequery_session_id"] = response["sessionId"]
+            st.session_state.menu_filequery_messages.append(
                 {
                     "role": "assistant",
                     "content": response["output"]["text"],
