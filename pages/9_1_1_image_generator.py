716aeca01e28f33e3fefd99f57a768a2ba8233dc
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-06-19 01:06:59 +0900

    image generator

--- pages/9_1_1_image_generator.py
+++ pages/9_1_1_image_generator.py
@@ -4,13 +4,16 @@ import cmn_settings
 import json
 import logging
 import cmn_auth
-import pyperclip
+import base64
+import io
+from PIL import Image
 import os
 from io import BytesIO
 import sys
 import subprocess
 from contextlib import closing
 from tempfile import gettempdir
+import random
 
 from pydub import AudioSegment
 from pydub.playback import play
@@ -35,6 +38,19 @@ bedrock_runtime = boto3.client('bedrock-runtime', region_name=AWS_REGION)
 polly = boto3.client("polly", region_name=AWS_REGION)
 
 ####################################################################################
+
+
+### Utilities
+
+def image_to_base64(image):
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+def base64_to_image(base64_str) -> Image:
+    return Image.open(io.BytesIO(base64.decodebytes(bytes(base64_str, "utf-8"))))
+
+#####################
 
 st.set_page_config(
     page_title="Image Generator",
@@ -96,73 +112,27 @@ st.markdown(
 )
 
 
-def copy_button_clicked(text):
-    pyperclip.copy(text)
-    #st.session_state.button = not st.session_state.button
-
-def recite_button_clicked(text):
-    try:
-        # Request speech synthesis
-        response = polly.synthesize_speech(Text=text, OutputFormat="mp3", VoiceId="Joanna")
-    except (BotoCoreError, ClientError) as error:
-        print(error)
-        return
-
-    if "AudioStream" in response:
-        # Note: Closing the stream is important because the service throttles on the
-        # number of parallel connections. Here we are using contextlib.closing to
-        # ensure the close method of the stream object will be called automatically
-        # at the end of the with statement's scope.
-            with closing(response["AudioStream"]) as stream:
-                output = os.path.join(gettempdir(), "speech.mp3")
-                try:
-                    # Open a file for writing the output as a binary stream
-                    sound = stream.read()
-                    with open(output, "wb") as file:
-                        file.write(sound)
-                    
-                    st.session_state['audio_stream'] = sound
-
-                    #data = open(output, 'rb').read()
-                    #song = AudioSegment.from_file(BytesIO(data), format="mp3")
-                    #play(song)
-                except IOError as error:
-                    # Could not write to file, exit gracefully
-                    print(error)
-                    #sys.exit(-1)
-                    st.session_state['audio_stream'] = ""
-                    return
-                
-                print("**********************************************************************")
-                try:                 
-                    print(f"/n/n---------------------------------------------------------/n{output}")   
-                    #data = open(output, 'rb').read()
-                    #print("------")
-                    #song = AudioSegment.from_file(BytesIO(data), format="mp3")
-                    #print("------")
-                    #play(song)
-                except IOError as error:
-                    print(error)
-                    return                
-
-    else:
-        # The response didn't contain audio data, exit gracefully
-        print("Could not stream audio")
-        #sys.exit(-1)
-        return
-       
-
 opt_model_id_list = [
-    "anthropic.claude-3-sonnet-20240229-v1:0",
-    "anthropic.claude-3-haiku-20240307-v1:0"
+    "stability.stable-diffusion-xl-v1"
+]
+
+opt_style_preset_list = [
+    "anime", 
+    "photographic"
+]
+
+opt_negative_prompt_list = [
+    "ugly", "tiling", "out of frame",
+    "disfigured", "deformed", "bad anatomy", "cut off", "low contrast", 
+    "underexposed", "overexposed", "bad art", "beginner", "amateur", "blurry", "draft", "grainy"
 ]
 
 with st.sidebar:
     opt_model_id = st.selectbox(label="Model ID", options=opt_model_id_list, index = 0, key="model_id")
-    opt_temperature = st.slider(label="Temperature", min_value=0.0, max_value=1.0, value=0.1, step=0.1, key="temperature")
-    opt_top_p = st.slider(label="Top P", min_value=0.0, max_value=1.0, value=1.0, step=0.1, key="top_p")
-    opt_top_k = st.slider(label="Top K", min_value=0, max_value=500, value=250, step=1, key="top_k")
-    opt_max_tokens = st.slider(label="Max Tokens", min_value=0, max_value=4096, value=2048, step=1, key="max_tokens")
+    opt_config_scale = st.slider(label="Config Scale", min_value=0, max_value=35, value=10, step=1, key="config_scale")
+    opt_steps = st.slider(label="Steps", min_value=10, max_value=50, value=30, step=1, key="steps")
+    opt_style_preset = st.selectbox(label="Style Presets", options=opt_style_preset_list, index = 0, key="style_preset")
+    opt_negative_prompt = st.multiselect(label="Negative Prompt", options=opt_negative_prompt_list, default=opt_negative_prompt_list, key="negative_prompt")
     opt_system_msg = st.text_area(label="System Message", value="", key="system_msg")
 
 
@@ -170,8 +140,8 @@ with st.sidebar:
 st.title("💬 Image Generator")
 st.write("Text to Image")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
+if "menu_img_gen_messages" not in st.session_state:
+    st.session_state["menu_img_gen_messages"] = [
         #{"role": "user", "content": "Hello there."},
         #{"role": "assistant", "content": "How can I help you?"}
     ]
@@ -180,22 +150,24 @@ if "messages" not in st.session_state:
 #    st.session_state["audio_stream"] = ""
 
 idx = 1
-for msg in st.session_state.messages:
+for msg in st.session_state.menu_img_gen_messages:
     idx = idx + 1
     content = msg["content"]
     with st.chat_message(msg["role"]):
-        st.write(content)
+        if "user" == msg["role"]:
+            st.write(content)
         if "assistant" == msg["role"]:
             #assistant_cmd_panel_col1, assistant_cmd_panel_col2, assistant_cmd_panel_col3 = st.columns([0.07,0.23,0.7], gap="small")
             #with assistant_cmd_panel_col2:
-            st.button(key=f"copy_button_{idx}", label='📄', type='primary', on_click=copy_button_clicked, args=[content])
+            #st.button(key=f"copy_button_{idx}", label='📄', type='primary', on_click=copy_button_clicked, args=[content])
+            st.image(content)
 
 
 if prompt := st.chat_input():
-    
-    st.session_state["audio_stream"] = ""
 
-    message_history = st.session_state.messages.copy()
+    #st.session_state["audio_stream"] = ""
+
+    message_history = st.session_state.menu_img_gen_messages.copy()
     message_history.append({"role": "user", "content": prompt})
     #st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
@@ -205,104 +177,44 @@ if prompt := st.chat_input():
     #print(f"messages={st.session_state.messages}")
 
     request = {
-        "anthropic_version": "bedrock-2023-05-31",
-        "temperature": opt_temperature,
-        "top_p": opt_top_p,
-        "top_k": opt_top_k,
-        "max_tokens": opt_max_tokens,
-        "system": opt_system_msg,
-        "messages": message_history #st.session_state.messages
-    }
+            "text_prompts": (
+                [{"text": prompt, "weight": 1.0}]
+                + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt]
+            ),
+            "cfg_scale": opt_config_scale,
+            #"clip_guidance_preset"
+            #"height": "1024",
+            #"width": "1024",
+            "seed": random.randint(0, 4294967295), # The seed determines the initial noise setting.0-4294967295,0
+            #"start_schedule": config["start_schedule"],
+            "steps": opt_steps, # Generation step determines how many times the image is sampled. 10-50,50
+            "style_preset": opt_style_preset,
+            "samples": 1,
+        }
     json.dumps(request, indent=3)
 
     try:
         #bedrock_model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
-        response = bedrock_runtime.invoke_model_with_response_stream(
+        response = bedrock_runtime.invoke_model(
             modelId = opt_model_id, #bedrock_model_id, 
             contentType = "application/json", #guardrailIdentifier  guardrailVersion=DRAFT, trace=ENABLED | DISABLED
             accept = "application/json",
             body = json.dumps(request))
+        
+        response_body = json.loads(response.get("body").read())
+        response_image_base64 = response_body["artifacts"][0].get("base64")
+        response_image:Image = base64_to_image(response_image_base64)
 
-        #with st.chat_message("assistant", avatar=setAvatar("assistant")):
-        result_text = ""
+        #file_extension = ".png"
+        #OUTPUT_IMG_PATH = os.path.join("./output/{}-{}{}".format("img", idx, file_extension))
+        #print("OUTPUT_IMG_PATH: " + OUTPUT_IMG_PATH)
+        #response_image.save(OUTPUT_IMG_PATH)
+
         with st.chat_message("assistant"):
-            result_container = st.container(border=True)
-            result_area = st.empty()
-            stream = response["body"]
-            for event in stream:
-                
-                if event["chunk"]:
-
-                    chunk = json.loads(event["chunk"]["bytes"])
-
-                    if chunk['type'] == 'message_start':
-                        opts = f"| temperature={opt_temperature} top_p={opt_top_p} top_k={opt_top_k} max_tokens={opt_max_tokens}"
-                        #result_text += f"{opts}\n\n"
-                        #result_area.write(result_text)
-                        result_container.write(opts)
-                        #pass
-
-                    elif chunk['type'] == 'message_delta':
-                        #print(f"\nStop reason: {chunk['delta']['stop_reason']}")
-                        #print(f"Stop sequence: {chunk['delta']['stop_sequence']}")
-                        #print(f"Output tokens: {chunk['usage']['output_tokens']}")
-                        pass
-
-                    elif chunk['type'] == 'content_block_delta':
-                        if chunk['delta']['type'] == 'text_delta':
-                            text = chunk['delta']['text']
-                            #await msg.stream_token(f"{text}")
-                            result_text += f"{text}"
-                            result_area.write(result_text)
-
-                    elif chunk['type'] == 'message_stop':
-                        invocation_metrics = chunk['amazon-bedrock-invocationMetrics']
-                        input_token_count = invocation_metrics["inputTokenCount"]
-                        output_token_count = invocation_metrics["outputTokenCount"]
-                        latency = invocation_metrics["invocationLatency"]
-                        lag = invocation_metrics["firstByteLatency"]
-                        stats = f"| token.in={input_token_count} token.out={output_token_count} latency={latency} lag={lag}"
-                        #await msg.stream_token(f"\n\n{stats}")
-                        #result_text += f"\n\n{stats}"
-                        #result_area.write(result_text)
-                        result_container.write(stats)
-
-                elif event["internalServerException"]:
-                    exception = event["internalServerException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["modelStreamErrorException"]:
-                    exception = event["modelStreamErrorException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["modelTimeoutException"]:
-                    exception = event["modelTimeoutException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["throttlingException"]:
-                    exception = event["throttlingException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                elif event["validationException"]:
-                    exception = event["validationException"]
-                    result_text += f"\n\{exception}"
-                    result_area.write(result_text)
-                else:
-                    result_text += f"\n\nUnknown Token"
-                    result_area.write(result_text)
-
-            col1, col2, col3 = st.columns([1,1,5])
-
-            with col1:
-                st.button(key='copy_button', label='📄', type='primary', on_click=copy_button_clicked, args=[result_text])
-            with col2:
-                if "audio_stream" not in st.session_state or st.session_state["audio_stream"] == "":
-                    st.button(key='recite_button', label='▶️', type='primary', on_click=recite_button_clicked, args=[result_text])
-            with col3:
-                st.markdown('3')
+            st.image(response_image)
             
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.session_state.messages.append({"role": "assistant", "content": result_text})
+        st.session_state.menu_img_gen_messages.append({"role": "user", "content": prompt})
+        st.session_state.menu_img_gen_messages.append({"role": "assistant", "content": response_image})
 
     except ClientError as err:
         message = err.response["Error"]["Message"]
@@ -310,6 +222,3 @@ if prompt := st.chat_input():
         print("A client error occured: " + format(message))
         st.chat_message("system").write(message)
 
-if "audio_stream" in st.session_state and st.session_state["audio_stream"] != "":
-    audio_bytes = BytesIO(st.session_state['audio_stream'])
-    st.audio(audio_bytes, format='audio/mp3', autoplay=False)
