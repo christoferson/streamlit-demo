e214c6faee59f1523b087431b088366d17a8081c
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-06-28 08:57:41 +0900

    translate

--- pages/7_3_1_translate.py
+++ pages/7_3_1_translate.py
@@ -4,7 +4,7 @@ import cmn_settings
 import cmn_st_audio
 import json
 import logging
-import cmn_auth
+import cmn_constants
 from io import BytesIO
 import textwrap as tw
 #from streamlit_extras.stylable_container import stylable_container
@@ -39,90 +39,9 @@ st.set_page_config(
 
 st.logo(icon_image="images/logo.png", image="images/logo_text.png")
 
-st.markdown(
-    """
-    <style>
-    button[kind="primary"] {
-        background: none!important;
-        border: none;
-        padding: 0!important;
-        margin: 0;
-        color: black !important;
-        text-decoration: none;
-        cursor: pointer;
-        border: none !important;
-    }
-    button[kind="primary"]:hover {
-        text-decoration: none;
-        color: black !important;
-    }
-    button[kind="primary"]:focus {
-        outline: none !important;
-        box-shadow: none !important;
-        color: black !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+#st.markdown(cmn_constants.css_sidebar,unsafe_allow_html=True)
 
-# Style for Code Block
-st.markdown(
-    """
-    <style>
-    div.stCodeBlock {
-        background-color: transparent;
-    }
-    div.stCodeBlock > pre {
-        background-color: transparent;
-    }
-    code.language-wiki {
-        font-size: 16px;
-        font-family: "Source Sans Pro", sans-serif;
-        line-height: 1.6;
-        max-width: 100%;
-        display: inline-block;
-        word-wrap: break-word;
-        word-break: break-all;
-        white-space: pre-line;
-        overflow-wrap: anywhere;
-        color: blue;
-        background-color: transparent;
-    }
-    code.language-wiki > span {
-        font-size: 16px;;
-        font-family: "Source Sans Pro", sans-serif;
-        line-height: 1.6;
-        max-width: 100%;
-        display: inline-block;
-        word-wrap: break-word;
-        word-break: break-all;
-        white-space: pre-line;
-        overflow-wrap: anywhere;
-        color: blue;
-        background-color: transparent;
-    }
-
-    code.language-markdown {
-        font-size: 16px;
-        font-family: "Source Sans Pro", sans-serif;
-        line-height: 1.6;
-        max-width: 100%;
-        display: inline-block;
-        word-wrap: break-word;
-        word-break: break-all;
-        white-space: pre-line;
-        overflow-wrap: anywhere;
-        color: orange;
-        background-color: transparent;
-    }
-
-    
-
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+#st.markdown(cmn_constants.css_sidebar_2,unsafe_allow_html=True)
 
 opt_model_id_list = [
     "anthropic.claude-3-sonnet-20240229-v1:0",
@@ -136,7 +55,7 @@ With regards to the target language, if it is not explicitly specified, assume t
 With regards to the output format, omit preambles and just provide the translated text.
 """
 
-
+error_translate_input_empty = ":red[Enter Source Text to Translate]"
 
 with st.sidebar:
     opt_model_id = st.selectbox(label="Model ID", options=opt_model_id_list, index = 0, key="model_id")
@@ -144,7 +63,6 @@ with st.sidebar:
     opt_top_p = st.slider(label="Top P", min_value=0.0, max_value=1.0, value=1.0, step=0.1, key="top_p")
     opt_top_k = st.slider(label="Top K", min_value=0, max_value=500, value=250, step=1, key="top_k")
     opt_max_tokens = st.slider(label="Max Tokens", min_value=0, max_value=4096, value=2048, step=1, key="max_tokens")
-    #opt_system_msg = st.text_area(label="System Message", value=system_message, key="system_msg")
 
 bedrock_runtime = boto3.client('bedrock-runtime', region_name=AWS_REGION)
 
@@ -154,55 +72,54 @@ if "translate_input" not in st.session_state:
 if "translate_result" not in st.session_state:
     st.session_state["translate_result"] = None
 
-
-def on_recite_button_clicked(text):
-    languages = cmn_st_audio.detect_dominant_language(text)
-    language = languages[0]["LanguageCode"] #ja
-    st.session_state['result_text_language'] = language
-
-    audio_binary = cmn_st_audio.synthesize_speech(text, language)
-    if audio_binary != None:
-        st.session_state['audio_stream'] = audio_binary
-    else:
-        st.session_state['audio_stream'] = None       
-
-
 st.title("💬 Translate v 7.3.1")
-#st.markdown("Enter text to translate")
 
 col1, col2 = st.columns(2)
 
 col2_container = col2.container()
 col2_container.caption(":blue[Result]")
 result_container = col2_container.container()
-#result_area = result_container.empty()
+#if "translate_result" in st.session_state and st.session_state["translate_result"] != None:
+#    result_display_columns = result_container.slider("columns", value=45, min_value=50, max_value=80, step=1, label_visibility="collapsed")
+#    result_text = st.session_state["translate_result"]
+#    result_text_wrapped = "\n".join(
+#        tw.wrap(result_text, width=result_display_columns, drop_whitespace=True, replace_whitespace=False)
+#    )
+#    #result_area.markdown(result_text)
+#    result_container.code(result_text_wrapped, language="markdown")
+#    markdown_display = result_container.checkbox("Markdown", value=True)
+#    if markdown_display:
+#        result_container.markdown(result_text)
+#    else:
+#        result_container.code(result_text, language="wiki")
+
+#if "translate_result_error" in st.session_state and st.session_state["translate_result_error"] != None:
+#    result_container.markdown(st.session_state["translate_result_error"])
+
 if "translate_result" in st.session_state and st.session_state["translate_result"] != None:
-    result_display_columns = result_container.slider("columns", value=45, min_value=50, max_value=80, step=1, label_visibility="collapsed")
-    result_text = st.session_state["translate_result"]
-    result_text_wrapped = "\n".join(
-        tw.wrap(result_text, width=result_display_columns, drop_whitespace=True, replace_whitespace=False)
-    )
-    #result_area.markdown(result_text)
-    result_container.code(result_text_wrapped, language="markdown")
-    markdown_display = result_container.checkbox("Markdown", value=True)
-    if markdown_display:
-        result_container.markdown(result_text)
-    else:
-        result_container.code(result_text, language="wiki")
+    translate_result_text = st.session_state["translate_result"]
+    if translate_result_text != "" and translate_result_text != error_translate_input_empty:
+        #result_container.markdown(translate_result_text)
+        pass
 
 
-result_columns = result_container.columns([1,1,1,1,1,1,1,1,1,1,1,1,1], gap="small")
-if "translate_result" in st.session_state and st.session_state["translate_result"] != None:
-    result_columns[0].button(key='copy_button', label='📄 Copy', type='primary', use_container_width=True)
-    result_columns[1].download_button(key="save_button", label='📩 Save', type='primary', file_name="result.txt", data=st.session_state["translate_result"], mime='text/csv', use_container_width=True)
-    result_columns[2].button(key="recite_button", label='Play', type='primary', on_click=on_recite_button_clicked, args=[st.session_state["translate_result"]], help="Text to Speech")
-if "audio_stream" in st.session_state and st.session_state["audio_stream"] != None:
-    audio_bytes = BytesIO(st.session_state['audio_stream'])
-    if "result_text_language" in st.session_state and st.session_state['result_text_language'] != None:
-        result_container.markdown(st.session_state['result_text_language'])
-    result_container.audio(audio_bytes, format='audio/mp3', autoplay=False)
+#result_columns = result_container.columns([1,1,1,1,1,1,1,1,1,1,1,1,1], gap="small")
+#if "translate_result" in st.session_state and st.session_state["translate_result"] != None:
+#    translate_result_text = st.session_state["translate_result"]
+#    if translate_result_text != "" and translate_result_text != error_translate_input_empty:
+#        result_columns[0].download_button(key="save_button", label='📩 Save', type='primary', file_name="result.txt", data=st.session_state["translate_result"], mime='text/csv', use_container_width=True)
 
 
+#result_columns = result_container.columns([1,1,1,1,1,1,1,1,1,1,1,1,1], gap="small")
+#if "translate_result" in st.session_state and st.session_state["translate_result"] != None:
+#    result_columns[0].button(key='copy_button', label='📄 Copy', type='primary', use_container_width=True)
+#    result_columns[1].download_button(key="save_button", label='📩 Save', type='primary', file_name="result.txt", data=st.session_state["translate_result"], mime='text/csv', use_container_width=True)
+#    result_columns[2].button(key="recite_button", label='Play', type='primary', args=[st.session_state["translate_result"]], help="Text to Speech")
+#if "audio_stream" in st.session_state and st.session_state["audio_stream"] != None:
+#    audio_bytes = BytesIO(st.session_state['audio_stream'])
+#    if "result_text_language" in st.session_state and st.session_state['result_text_language'] != None:
+#        result_container.markdown(st.session_state['result_text_language'])
+#    result_container.audio(audio_bytes, format='audio/mp3', autoplay=False)
 
 def on_text_area_translate_input_changed():
     pass
@@ -217,7 +134,9 @@ col1.text_area(
         help="Specify the Target Language or Custom Instructions at the end. e.g. <input_text> --> Formal English"
     )
 
-def stream_data(stream):
+def stream_result_data(stream):
+
+    result_text = ""
 
     try: 
 
@@ -237,9 +156,8 @@ def stream_data(stream):
                 elif chunk['type'] == 'content_block_delta':
                     if chunk['delta']['type'] == 'text_delta':
                         text = chunk['delta']['text']
+                        result_text += f"{text}"
                         yield text
-                        #result_text += f"{text}"
-                        #result_area.write(result_text)
 
                 elif chunk['type'] == 'message_stop':
                     invocation_metrics = chunk['amazon-bedrock-invocationMetrics']
@@ -271,6 +189,8 @@ def stream_data(stream):
                 yield exception
             else:
                 yield "Unknown Token"
+
+        st.session_state["translate_result"] = result_text
     
     except ClientError as err:
         message = err.response["Error"]["Message"]
@@ -281,18 +201,23 @@ def stream_data(stream):
 def on_button_clear_clicked():
     st.session_state["translate_input"] = ""
     st.session_state["translate_result"] = None
-    st.session_state['result_text_language'] = None
-    st.session_state['audio_stream'] = None
+    #st.session_state['result_text_language'] = None
+    #st.session_state['audio_stream'] = None
 
 def on_button_translate_clicked():
     if "translate_input" not in  st.session_state or st.session_state["translate_input"] == "":
-        st.session_state["translate_result"] = ":red[Enter Source Text to Translate]"
+        st.session_state["translate_result_error"] = error_translate_input_empty
+        result_container.markdown(error_translate_input_empty)
         return
 
-    st.session_state['result_text_language'] = None
-    st.session_state['audio_stream'] = None
+    if "translate_result_error" in st.session_state:
+        del st.session_state["translate_result_error"]
+    
+    if "translate_result" in st.session_state:
+        del st.session_state["translate_result"]
 
-    #result_area.write("...")
+    #st.session_state['result_text_language'] = None
+    #st.session_state['audio_stream'] = None
 
     prompt = st.session_state["translate_input"]
 
@@ -308,9 +233,8 @@ def on_button_translate_clicked():
             "messages": message_history
         }
     
-    json.dumps(request, indent=3)
-    
     try:
+
         response = bedrock_runtime.invoke_model_with_response_stream(
             modelId = opt_model_id, #bedrock_model_id, 
             contentType = "application/json", #guardrailIdentifier  guardrailVersion=DRAFT, trace=ENABLED | DISABLED
@@ -318,7 +242,14 @@ def on_button_translate_clicked():
             body = json.dumps(request))
         
         stream = response["body"]
-        result_container.write_stream(stream_data(stream))
+        result_container.write_stream(stream_result_data(stream))
+        result_columns = result_container.columns([1,1,1,1,1,1,1,1,1,1,1,1,1], gap="small")
+        if "translate_result" in st.session_state and st.session_state["translate_result"] != None:
+            translate_result_text = st.session_state["translate_result"]
+            if translate_result_text != "" and translate_result_text != error_translate_input_empty:
+                result_columns[0].download_button(key="save_button", label='📩 Save', type='primary', file_name="result.txt", data=st.session_state["translate_result"], mime='text/csv', use_container_width=True)
+
+
         
     except ClientError as err:
         message = err.response["Error"]["Message"]
@@ -330,7 +261,3 @@ def on_button_translate_clicked():
 button_panel = col1.columns([1,1,1,1,1,1,1,1,1], gap="small") #gap ("small", "medium", or "large")
 button_panel[8].button("Translate", on_click=on_button_translate_clicked, use_container_width=True)
 button_panel[7].button("⎚ Clear", on_click=on_button_clear_clicked, use_container_width=True)
-
-
-
-
