81e466e7e6c788e59275bfb465c218d2cd6b508b
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-08-04 00:45:43 +0900

    image variation

--- pages/9_3_6_image_variation.py
+++ pages/9_3_6_image_variation.py
@@ -7,6 +7,7 @@ import logging
 import cmn_auth
 import base64
 import io
+import PIL
 from PIL import Image
 import os
 from io import BytesIO
@@ -68,6 +69,15 @@ st.set_page_config(
 st.logo(icon_image="images/logo.png", image="images/logo_text.png")
 
 st.markdown(cmn_constants.css_btn_primary, unsafe_allow_html=True)
+
+variation_prompts_init = [
+    "Trendy casual sneakers, comfortable design, vibrant colors, for young adults",
+    "Stylish high heel shoes, modern design, comfortable fit, for fashion-forward women",
+    "Rugged hiking boots, waterproof material, excellent traction, for outdoor enthusiasts",
+    "High-end designer shoes, premium materials, unique aesthetic, for fashion connoisseurs",
+    "Slip-resistant work boots, with steel toe protection for construction or industrial settings",
+    "Stylish and comfortable sandals for women, perfect for a beach vacation or resort wear",
+]
 
 opt_model_id_list = [ "stability.stable-diffusion-xl-v1" ]
 
@@ -140,7 +150,7 @@ if "menu_img_variation_messages" not in st.session_state:
 
 
 uploaded_file = st.file_uploader(
-    "",
+    "Upload Base Image",
     type=["PNG", "JPEG"],
     accept_multiple_files=False,
     label_visibility="collapsed",
@@ -154,20 +164,49 @@ if uploaded_file:
     uploaded_file_name = uploaded_file.name
     uploaded_file_type = uploaded_file.type
     uploaded_file_base64 = image_to_base64(uploaded_file_image, mime_mapping[uploaded_file_type])
-    st.image(
-        uploaded_file_image, caption='upload images',
-        use_column_width=True
+
+    # Get the original dimensions
+    original_width, original_height = uploaded_file_image.size
+
+    # Check if the original dimensions are multiples of 64
+    if original_width % 64 != 0 or original_height % 64 != 0:
+        # Calculate the new dimensions that are multiples of 64
+        new_width = (original_width + 63) // 64 * 64
+        new_height = (original_height + 63) // 64 * 64
+
+        # Resize the image
+        uploaded_file_image = uploaded_file_image.resize((new_width, new_height), PIL.Image.Resampling.LANCZOS)
+        uploaded_file_base64 = image_to_base64(uploaded_file_image, mime_mapping[uploaded_file_type])
+
+
+
+    st.image(uploaded_file_image, caption=f"Base Image {original_width}x{original_height}",
+        use_column_width="auto" #"auto", "always", "never", or bool
     )
     print(uploaded_file_type)
     #uploaded_file_bytes = uploaded_file.read()
     #uploaded_file_base64 = base64.b64encode(uploaded_file_bytes).decode("utf-8")
     #uploaded_file_base64 = base64.b64encode(uploaded_file_bytes)
 
+# Join the elements with a newline character
+variation_prompts_init_str = "\n".join(variation_prompts_init)
+
+# Display the text area with the joined string
+variation_prompts_str = st.text_area(":blue[**Variation Prompts**]", value=variation_prompts_init_str, height=170, max_chars=2000,
+                                     placeholder="Enter each variation as a separate line")
+
 generate_btn = st.button("Generate", type="primary", disabled=uploaded_file_name==None)
 
 #if prompt := st.chat_input(disabled=uploaded_file_name==None):
 
 if generate_btn:
+
+        
+    # Split the string into lines
+    variation_prompts_lines = variation_prompts_str.split("\n")
+
+    # Take only the first 6 lines
+    variation_prompts = [line.strip() for line in variation_prompts_lines[:6] if line.strip()]
 
     #message_history = st.session_state.menu_img_variation_messages.copy()
     #message_history.append({"role": "user", "content": prompt})
@@ -191,74 +230,81 @@ if generate_btn:
 
     #json.dumps(request, indent=3)
 
-    variation_prompts = [
-        "Trendy casual sneakers, comfortable design, vibrant colors, for young adults",
-        "Stylish high heel shoes, modern design, comfortable fit, for fashion-forward women",
-        #"Rugged hiking boots, waterproof material, excellent traction, for outdoor enthusiasts",
-        #"High-end designer shoes, premium materials, unique aesthetic, for fashion connoisseurs",
-    ]
+
 
     with st.spinner('Generating Image...'):
 
-        
-        try:
+        with st.container():
 
-            for variation_prompt in variation_prompts:
+            # Print commmon request properties
+            current_datetime = datetime.now()
+            current_datetime_str = current_datetime.strftime("%Y/%m/%d, %H:%M:%S")
+            st.markdown(f":blue[**style**] {opt_style_preset} :blue[**seed**] {seed} :blue[**scale**] {opt_config_scale} :blue[**steps**] {opt_steps} :blue[**width**] {opt_dimensions_width} :blue[**height**] {opt_dimensions_height} :green[**{current_datetime_str}**]")
+            
+            cols = st.columns(3)
+            
+            try:
+
+                for i, variation_prompt in enumerate(variation_prompts):
+
+                    # Determine the column to place the image in
+                    col_index = i % 3
+                        
+                    request = {
+                            "text_prompts": (
+                                #[{"text": prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt]
+                                [{"text": variation_prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt_elements]
+                            ),
+                            "cfg_scale": opt_config_scale,
+                            #"clip_guidance_preset"
+                            "width": opt_dimensions_width,
+                            "height": opt_dimensions_height,
+                            "seed": seed, #random.randint(0, 4294967295),
+                            #"start_schedule": config["start_schedule"],
+                            "steps": opt_steps, # Generation step determines how many times the image is sampled. 10-50,50
+                            "style_preset": opt_style_preset,
+                            "samples": 1,
+                            "init_image": uploaded_file_base64,
+                            "init_image_mode": "IMAGE_STRENGTH",
+                            #"image_strength": 1,
+                        }
+
+                    response = bedrock_runtime.invoke_model(
+                        modelId = opt_model_id,
+                        contentType = "application/json", #guardrailIdentifier  guardrailVersion=DRAFT, trace=ENABLED | DISABLED
+                        accept = "application/json",
+                        body = json.dumps(request))
                     
-                request = {
-                        "text_prompts": (
-                            #[{"text": prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt]
-                            [{"text": variation_prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt_elements]
-                        ),
-                        "cfg_scale": opt_config_scale,
-                        #"clip_guidance_preset"
-                        "width": opt_dimensions_width,
-                        "height": opt_dimensions_height,
-                        "seed": seed, #random.randint(0, 4294967295),
-                        #"start_schedule": config["start_schedule"],
-                        "steps": opt_steps, # Generation step determines how many times the image is sampled. 10-50,50
-                        "style_preset": opt_style_preset,
-                        "samples": 1,
-                        "init_image": uploaded_file_base64,
-                        "init_image_mode": "IMAGE_STRENGTH",
-                        #"image_strength": 1,
-                    }
+                    response_body = json.loads(response.get("body").read())
+                    finish_reason = response_body.get("artifacts")[0].get("finishReason")
 
-                response = bedrock_runtime.invoke_model(
-                    modelId = opt_model_id,
-                    contentType = "application/json", #guardrailIdentifier  guardrailVersion=DRAFT, trace=ENABLED | DISABLED
-                    accept = "application/json",
-                    body = json.dumps(request))
-                
-                response_body = json.loads(response.get("body").read())
-                finish_reason = response_body.get("artifacts")[0].get("finishReason")
-                if finish_reason == 'ERROR' or finish_reason == 'CONTENT_FILTERED':
-                    st.chat_message("system").write(f"Image generation error. Error code is {finish_reason}")
-                else:
-                    response_image_base64 = response_body["artifacts"][0].get("base64")
-                    response_image:Image = base64_to_image(response_image_base64)
+                    # Display the image and metadata in the corresponding column
+                    with cols[col_index]:
 
-                    with st.chat_message("assistant"):
-                        current_datetime = datetime.now()
-                        current_datetime_str = current_datetime.strftime("%Y/%m/%d, %H:%M:%S")
-                        st.image(response_image)
-                        st.markdown(f":blue[**style**] {opt_style_preset} :blue[**seed**] {seed} :blue[**scale**] {opt_config_scale} :blue[**steps**] {opt_steps} :blue[**width**] {opt_dimensions_width} :blue[**height**] {opt_dimensions_height} :green[**{current_datetime_str}**]")
+                        if finish_reason == 'ERROR' or finish_reason == 'CONTENT_FILTERED':
+                            st.markdown(f"Image generation error. Error code is {finish_reason}")
+                        else:
+                            response_image_base64 = response_body["artifacts"][0].get("base64")
+                            response_image:Image = base64_to_image(response_image_base64)
+                            st.image(response_image)
+                            st.markdown(f":orange[*{variation_prompt}*]")
+                            
 
-                #st.session_state.menu_img_variation_messages.append({"role": "user", "content": prompt})
-                #st.session_state.menu_img_variation_messages.append({"role": "assistant", 
-                #    "content": response_image, 
-                #    "style": opt_style_preset,
-                #    "seed": seed,
-                #    "scale": opt_config_scale,
-                #    "steps": opt_steps,
-                #    "width": opt_dimensions_width,
-                #    "height": opt_dimensions_height,
-                #})
+                    #st.session_state.menu_img_variation_messages.append({"role": "user", "content": prompt})
+                    #st.session_state.menu_img_variation_messages.append({"role": "assistant", 
+                    #    "content": response_image, 
+                    #    "style": opt_style_preset,
+                    #    "seed": seed,
+                    #    "scale": opt_config_scale,
+                    #    "steps": opt_steps,
+                    #    "width": opt_dimensions_width,
+                    #    "height": opt_dimensions_height,
+                    #})
 
-        except ClientError as err:
-            message = err.response["Error"]["Message"]
-            logger.error("A client error occurred: %s", message)
-            print("A client error occured: " + format(message))
-            st.chat_message("system").write(message)
+            except ClientError as err:
+                message = err.response["Error"]["Message"]
+                logger.error("A client error occurred: %s", message)
+                print("A client error occured: " + format(message))
+                st.chat_message("system").write(message)
 
 
