51863b884c9607f3475ee1eb60119a67cd8fe04d
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-08-16 19:58:55 +0900

    image generate

--- pages/9_1_6_image_generator.py
+++ pages/9_1_6_image_generator.py
@@ -88,26 +88,6 @@ st.markdown(
 )
 
 
-###########################
-
-def update_menu_img_gen_splash():
-    if st.session_state["menu_img_gen_splash"] == True:
-        st.session_state["menu_img_gen_splash"] = False
-    else:
-        st.session_state["menu_img_gen_splash"] = True
-
-if "menu_img_gen_splash" not in st.session_state:
-    st.session_state["menu_img_gen_splash"] = True
-
-if st.session_state["menu_img_gen_splash"] == True:
-    st.markdown("Sample Prompt 1")
-    st.markdown("Sample Prompt 2")
-    st.button("Close", type="primary", on_click=update_menu_img_gen_splash)
-else:
-    st.button("Again", type="primary", on_click=update_menu_img_gen_splash)
-
-###########################
-
 opt_model_id_list = [
     "stability.stable-diffusion-xl-v1"
 ]
@@ -159,114 +139,117 @@ with st.sidebar:
     opt_seed = st.slider(label=":blue[**Seed**]", min_value=-1, max_value=4294967295, value=-1, step=1, key="seed")
     opt_negative_prompt_csv = st.text_area(label=":blue[**Negative Prompts**]", value="", placeholder="Things you don't want to see in the generated image. Input comma separated values. e.g. ugly,disfigured,low contrast,underexposed,overexposed,blurry,grainy", max_chars=256, key="negative_prompts")
 
+tab_basic, tab_reference_image = st.tabs(["Basic", "Reference Image"])
 
 
-#st.title("💬 🖌️ 🖼️ Image Generator")
-st.markdown("🖼️ Image Generator 6")
-#st.write("Text to Image")
+with tab_basic:
 
-if "menu_img_gen_messages" not in st.session_state:
-    st.session_state["menu_img_gen_messages"] = [
-        #{"role": "user", "content": "Hello there."},
-        #{"role": "assistant", "content": "How can I help you?"}
-    ]
+    #st.title("💬 🖌️ 🖼️ Image Generator")
+    st.markdown("🖼️ Image Generator 6")
+    #st.write("Text to Image")
 
-idx = 1
-for msg in st.session_state.menu_img_gen_messages:
-    idx = idx + 1
-    content = msg["content"]
-    with st.chat_message(msg["role"]):
-        if "user" == msg["role"]:
-            st.write(content)
-        if "assistant" == msg["role"]:
-            st.image(content)
-            st.markdown(f":blue[**style**] {msg['style']} :blue[**seed**] {msg['seed']} :blue[**scale**] {msg['scale']} :blue[**steps**] {msg['steps']} :blue[**width**] {msg['width']} :blue[**height**] {msg['height']}")
+    if "menu_img_gen_messages" not in st.session_state:
+        st.session_state["menu_img_gen_messages"] = [
+            #{"role": "user", "content": "Hello there."},
+            #{"role": "assistant", "content": "How can I help you?"}
+        ]
 
-if prompt := st.chat_input():
+    idx = 1
+    for msg in st.session_state.menu_img_gen_messages:
+        idx = idx + 1
+        content = msg["content"]
+        with st.chat_message(msg["role"]):
+            if "user" == msg["role"]:
+                st.write(content)
+            if "assistant" == msg["role"]:
+                st.image(content)
+                st.markdown(f":blue[**style**] {msg['style']} :blue[**seed**] {msg['seed']} :blue[**scale**] {msg['scale']} :blue[**steps**] {msg['steps']} :blue[**width**] {msg['width']} :blue[**height**] {msg['height']}")
 
-    st.session_state["menu_img_gen_splash"] = False
+    if prompt := st.chat_input():
 
-    message_history = st.session_state.menu_img_gen_messages.copy()
-    message_history.append({"role": "user", "content": prompt})
-    #st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+        st.session_state["menu_img_gen_splash"] = False
 
-    opt_dimensions_width = int(opt_dimensions.split("x")[0])
-    opt_dimensions_height = int(opt_dimensions.split("x")[1])
-    #print(f"width: {opt_dimensions_width}  height: {opt_dimensions_height}")
+        message_history = st.session_state.menu_img_gen_messages.copy()
+        message_history.append({"role": "user", "content": prompt})
+        #st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
 
-    opt_negative_prompt_elements = opt_negative_prompt_list
-    if "" != opt_negative_prompt_csv:
-        opt_negative_prompt_elements = opt_negative_prompt_csv.split(",")
-    print(opt_negative_prompt_elements)
-        
+        opt_dimensions_width = int(opt_dimensions.split("x")[0])
+        opt_dimensions_height = int(opt_dimensions.split("x")[1])
+        #print(f"width: {opt_dimensions_width}  height: {opt_dimensions_height}")
 
-    seed = opt_seed
-    if seed < 0:
-        seed = random.randint(0, 4294967295)
-    
-    logger.info(f"prompt={prompt} negative={opt_negative_prompt_csv}")
-
-    request = {
-            "text_prompts": (
-                #[{"text": prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt]
-                [{"text": prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt_elements]
-            ),
-            "cfg_scale": opt_config_scale,
-            #"clip_guidance_preset"
-            "width": opt_dimensions_width,
-            "height": opt_dimensions_height,
-            "seed": seed, #random.randint(0, 4294967295),
-            #"start_schedule": config["start_schedule"],
-            "steps": opt_steps, # Generation step determines how many times the image is sampled. 10-50,50
-            "style_preset": opt_style_preset,
-            "samples": 1,
-        }
-    #json.dumps(request, indent=3)
-
-    with st.spinner('Generating Image...'):
-
-        try:
-
-            response = bedrock_runtime.invoke_model(
-                modelId = opt_model_id,
-                contentType = "application/json", #guardrailIdentifier  guardrailVersion=DRAFT, trace=ENABLED | DISABLED
-                accept = "application/json",
-                body = json.dumps(request))
+        opt_negative_prompt_elements = opt_negative_prompt_list
+        if "" != opt_negative_prompt_csv:
+            opt_negative_prompt_elements = opt_negative_prompt_csv.split(",")
+        print(opt_negative_prompt_elements)
             
-            response_body = json.loads(response.get("body").read())
-            finish_reason = response_body.get("artifacts")[0].get("finishReason")
-            if finish_reason == 'ERROR' or finish_reason == 'CONTENT_FILTERED':
-                st.chat_message("system").write(f"Image generation error. Error code is {finish_reason}")
-            else:
-                response_image_base64 = response_body["artifacts"][0].get("base64")
-                response_image:Image = base64_to_image(response_image_base64)
 
-                #file_extension = ".png"
-                #OUTPUT_IMG_PATH = os.path.join("./output/{}-{}{}".format("img", idx, file_extension))
-                #print("OUTPUT_IMG_PATH: " + OUTPUT_IMG_PATH)
-                #response_image.save(OUTPUT_IMG_PATH)
+        seed = opt_seed
+        if seed < 0:
+            seed = random.randint(0, 4294967295)
+        
+        logger.info(f"prompt={prompt} negative={opt_negative_prompt_csv}")
 
-                with st.chat_message("assistant"):
-                    current_datetime = datetime.now()
-                    current_datetime_str = current_datetime.strftime("%Y/%m/%d, %H:%M:%S")
-                    st.image(response_image)
-                    st.markdown(f":blue[**style**] {opt_style_preset} :blue[**seed**] {seed} :blue[**scale**] {opt_config_scale} :blue[**steps**] {opt_steps} :blue[**width**] {opt_dimensions_width} :blue[**height**] {opt_dimensions_height} :green[**{current_datetime_str}**]")
+        request = {
+                "text_prompts": (
+                    #[{"text": prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt]
+                    [{"text": prompt, "weight": 1.0}] + [{"text": negprompt, "weight": -1.0} for negprompt in opt_negative_prompt_elements]
+                ),
+                "cfg_scale": opt_config_scale,
+                #"clip_guidance_preset"
+                "width": opt_dimensions_width,
+                "height": opt_dimensions_height,
+                "seed": seed, #random.randint(0, 4294967295),
+                #"start_schedule": config["start_schedule"],
+                "steps": opt_steps, # Generation step determines how many times the image is sampled. 10-50,50
+                "style_preset": opt_style_preset,
+                "samples": 1,
+            }
+        #json.dumps(request, indent=3)
 
-                st.session_state.menu_img_gen_messages.append({"role": "user", "content": prompt})
-                st.session_state.menu_img_gen_messages.append({"role": "assistant", 
-                    "content": response_image, 
-                    "style": opt_style_preset,
-                    "seed": seed,
-                    "scale": opt_config_scale,
-                    "steps": opt_steps,
-                    "width": opt_dimensions_width,
-                    "height": opt_dimensions_height,
-                })
+        with st.spinner('Generating Image...'):
 
-        except ClientError as err:
-            message = err.response["Error"]["Message"]
-            logger.error("A client error occurred: %s", message)
-            print("A client error occured: " + format(message))
-            st.chat_message("system").write(message)
+            try:
+
+                response = bedrock_runtime.invoke_model(
+                    modelId = opt_model_id,
+                    contentType = "application/json", #guardrailIdentifier  guardrailVersion=DRAFT, trace=ENABLED | DISABLED
+                    accept = "application/json",
+                    body = json.dumps(request))
+                
+                response_body = json.loads(response.get("body").read())
+                finish_reason = response_body.get("artifacts")[0].get("finishReason")
+                if finish_reason == 'ERROR' or finish_reason == 'CONTENT_FILTERED':
+                    st.chat_message("system").write(f"Image generation error. Error code is {finish_reason}")
+                else:
+                    response_image_base64 = response_body["artifacts"][0].get("base64")
+                    response_image:Image = base64_to_image(response_image_base64)
+
+                    #file_extension = ".png"
+                    #OUTPUT_IMG_PATH = os.path.join("./output/{}-{}{}".format("img", idx, file_extension))
+                    #print("OUTPUT_IMG_PATH: " + OUTPUT_IMG_PATH)
+                    #response_image.save(OUTPUT_IMG_PATH)
+
+                    with st.chat_message("assistant"):
+                        current_datetime = datetime.now()
+                        current_datetime_str = current_datetime.strftime("%Y/%m/%d, %H:%M:%S")
+                        st.image(response_image)
+                        st.markdown(f":blue[**style**] {opt_style_preset} :blue[**seed**] {seed} :blue[**scale**] {opt_config_scale} :blue[**steps**] {opt_steps} :blue[**width**] {opt_dimensions_width} :blue[**height**] {opt_dimensions_height} :green[**{current_datetime_str}**]")
+
+                    st.session_state.menu_img_gen_messages.append({"role": "user", "content": prompt})
+                    st.session_state.menu_img_gen_messages.append({"role": "assistant", 
+                        "content": response_image, 
+                        "style": opt_style_preset,
+                        "seed": seed,
+                        "scale": opt_config_scale,
+                        "steps": opt_steps,
+                        "width": opt_dimensions_width,
+                        "height": opt_dimensions_height,
+                    })
+
+            except ClientError as err:
+                message = err.response["Error"]["Message"]
+                logger.error("A client error occurred: %s", message)
+                print("A client error occured: " + format(message))
+                st.chat_message("system").write(message)
 
