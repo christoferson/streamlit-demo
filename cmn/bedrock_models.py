bd243739e90ae214f158fa5a1d1c7a1a3b7dba1c
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-10-03 01:21:21 +0900

    Add Provider

--- cmn/bedrock_models.py
+++ cmn/bedrock_models.py
@@ -1,5 +1,6 @@
 class FoundationModel:
-    def __init__(self, model_id, converse, converse_stream, system_prompts, document_chat, vision, tool_use, streaming_tool_use, guardrails):
+    def __init__(self, provider, model_id, converse, converse_stream, system_prompts, document_chat, vision, tool_use, streaming_tool_use, guardrails):
+        self.provider = provider
         self.model_id = model_id
         self.features = {
             'converse': converse,
@@ -15,40 +16,43 @@ class FoundationModel:
     def isFeatureSupported(self, feature_id):
         return self.features.get(feature_id, False)
 
+    def get_provider(self):
+        return self.provider
+
     @staticmethod
     def find(model_id):
         return foundation_models.get(model_id)
 
 # Dictionary to store FoundationModel instances
 foundation_models = {
-    "anthropic.claude-3-5-sonnet-20240620-v1:0": FoundationModel("anthropic.claude-3-5-sonnet-20240620-v1:0", True, True, True, True, True, True, True, True),
-    "anthropic.claude-3-sonnet-20240229-v1:0": FoundationModel("anthropic.claude-3-sonnet-20240229-v1:0", True, True, True, True, True, True, True, True),
-    "anthropic.claude-3-haiku-20240307-v1:0": FoundationModel("anthropic.claude-3-haiku-20240307-v1:0", True, True, True, True, True, True, True, True),
-    "us.anthropic.claude-3-haiku-20240307-v1:0": FoundationModel("us.anthropic.claude-3-haiku-20240307-v1:0", True, True, True, True, True, True, True, True),
-    "us.anthropic.claude-3-sonnet-20240229-v1:0": FoundationModel("us.anthropic.claude-3-sonnet-20240229-v1:0", True, True, True, True, True, True, True, True),
-    "us.anthropic.claude-3-opus-20240229-v1:0": FoundationModel("us.anthropic.claude-3-opus-20240229-v1:0", True, True, True, True, True, True, True, True),
-    "us.anthropic.claude-3-5-sonnet-20240620-v1:0": FoundationModel("us.anthropic.claude-3-5-sonnet-20240620-v1:0", True, True, True, True, True, True, True, True),
-    "cohere.command-r-v1:0": FoundationModel("cohere.command-r-v1:0", True, True, True, True, False, True, True, False),
-    "cohere.command-r-plus-v1:0": FoundationModel("cohere.command-r-plus-v1:0", True, True, True, True, False, True, True, False),
-    "meta.llama2-13b-chat-v1": FoundationModel("meta.llama2-13b-chat-v1", True, True, True, True, False, False, False, True),
-    "meta.llama2-70b-chat-v1": FoundationModel("meta.llama2-70b-chat-v1", True, True, True, True, False, False, False, True),
-    "meta.llama3-8b-instruct-v1:0": FoundationModel("meta.llama3-8b-instruct-v1:0", True, True, True, True, False, True, False, True),
-    "meta.llama3-70b-instruct-v1:0": FoundationModel("meta.llama3-70b-instruct-v1:0", True, True, True, True, False, True, False, True),
-    "us.meta.llama3-2-11b-instruct-v1:0": FoundationModel("us.meta.llama3-2-11b-instruct-v1:0", True, True, True, True, True, True, False, True),
-    "us.meta.llama3-2-90b-instruct-v1:0": FoundationModel("us.meta.llama3-2-90b-instruct-v1:0", True, True, True, True, True, True, False, True),
-    "mistral.mistral-small-2402-v1:0": FoundationModel("mistral.mistral-small-2402-v1:0", True, True, True, False, False, True, False, True),
-    "mistral.mistral-large-2402-v1:0": FoundationModel("mistral.mistral-large-2402-v1:0", True, True, True, True, False, True, False, True),
+    "anthropic.claude-3-5-sonnet-20240620-v1:0": FoundationModel("Anthropic", "anthropic.claude-3-5-sonnet-20240620-v1:0", True, True, True, True, True, True, True, True),
+    "anthropic.claude-3-sonnet-20240229-v1:0": FoundationModel("Anthropic", "anthropic.claude-3-sonnet-20240229-v1:0", True, True, True, True, True, True, True, True),
+    "anthropic.claude-3-haiku-20240307-v1:0": FoundationModel("Anthropic", "anthropic.claude-3-haiku-20240307-v1:0", True, True, True, True, True, True, True, True),
+    "us.anthropic.claude-3-haiku-20240307-v1:0": FoundationModel("Anthropic", "us.anthropic.claude-3-haiku-20240307-v1:0", True, True, True, True, True, True, True, True),
+    "us.anthropic.claude-3-sonnet-20240229-v1:0": FoundationModel("Anthropic", "us.anthropic.claude-3-sonnet-20240229-v1:0", True, True, True, True, True, True, True, True),
+    "us.anthropic.claude-3-opus-20240229-v1:0": FoundationModel("Anthropic", "us.anthropic.claude-3-opus-20240229-v1:0", True, True, True, True, True, True, True, True),
+    "us.anthropic.claude-3-5-sonnet-20240620-v1:0": FoundationModel("Anthropic", "us.anthropic.claude-3-5-sonnet-20240620-v1:0", True, True, True, True, True, True, True, True),
+    "cohere.command-r-v1:0": FoundationModel("Cohere", "cohere.command-r-v1:0", True, True, True, True, False, True, True, False),
+    "cohere.command-r-plus-v1:0": FoundationModel("Cohere", "cohere.command-r-plus-v1:0", True, True, True, True, False, True, True, False),
+    "meta.llama2-13b-chat-v1": FoundationModel("Meta", "meta.llama2-13b-chat-v1", True, True, True, True, False, False, False, True),
+    "meta.llama2-70b-chat-v1": FoundationModel("Meta", "meta.llama2-70b-chat-v1", True, True, True, True, False, False, False, True),
+    "meta.llama3-8b-instruct-v1:0": FoundationModel("Meta", "meta.llama3-8b-instruct-v1:0", True, True, True, True, False, True, False, True),
+    "meta.llama3-70b-instruct-v1:0": FoundationModel("Meta", "meta.llama3-70b-instruct-v1:0", True, True, True, True, False, True, False, True),
+    "us.meta.llama3-2-11b-instruct-v1:0": FoundationModel("Meta", "us.meta.llama3-2-11b-instruct-v1:0", True, True, True, True, True, True, False, True),
+    "us.meta.llama3-2-90b-instruct-v1:0": FoundationModel("Meta", "us.meta.llama3-2-90b-instruct-v1:0", True, True, True, True, True, True, False, True),
+    "mistral.mistral-small-2402-v1:0": FoundationModel("Mistral", "mistral.mistral-small-2402-v1:0", True, True, True, False, False, True, False, True),
+    "mistral.mistral-large-2402-v1:0": FoundationModel("Mistral", "mistral.mistral-large-2402-v1:0", True, True, True, True, False, True, False, True),
 
     # Models defined in the documentation but not in opt_model_id_list
-    "AI21 Jamba-Instruct": FoundationModel("AI21 Jamba-Instruct", True, True, True, False, False, False, False, False),
-    "AI21 Labs Jurassic-2 (Text)": FoundationModel("AI21 Labs Jurassic-2 (Text)", True, False, False, False, False, False, False, True),
-    "AI21 Labs Jamba 1.5 Large": FoundationModel("AI21 Labs Jamba 1.5 Large", True, True, True, True, False, True, True, True),
-    "AI21 Labs Jamba 1.5 Mini": FoundationModel("AI21 Labs Jamba 1.5 Mini", True, True, True, True, False, True, True, True),
-    "Amazon Titan models": FoundationModel("Amazon Titan models", True, True, False, True, False, False, False, True),
-    "Anthropic Claude 2.x and earlier models": FoundationModel("Anthropic Claude 2.x and earlier models", True, True, True, True, False, False, False, True),
-    "Cohere Command": FoundationModel("Cohere Command", True, True, False, True, False, False, False, True),
-    "Cohere Command Light": FoundationModel("Cohere Command Light", True, True, False, False, False, False, False, True),
-    "Mistral AI Instruct": FoundationModel("Mistral AI Instruct", True, True, False, True, False, False, False, True),
-    "Mistral Large 2 (24.07)": FoundationModel("Mistral Large 2 (24.07)", True, True, True, True, False, True, False, True),
-    "Mistral Small": FoundationModel("Mistral Small", True, True, True, False, False, True, False, True),
+    "AI21 Jamba-Instruct": FoundationModel("AI21", "AI21 Jamba-Instruct", True, True, True, False, False, False, False, False),
+    "AI21 Labs Jurassic-2 (Text)": FoundationModel("AI21", "AI21 Labs Jurassic-2 (Text)", True, False, False, False, False, False, False, True),
+    "AI21 Labs Jamba 1.5 Large": FoundationModel("AI21", "AI21 Labs Jamba 1.5 Large", True, True, True, True, False, True, True, True),
+    "AI21 Labs Jamba 1.5 Mini": FoundationModel("AI21", "AI21 Labs Jamba 1.5 Mini", True, True, True, True, False, True, True, True),
+    "Amazon Titan models": FoundationModel("Amazon", "Amazon Titan models", True, True, False, True, False, False, False, True),
+    "Anthropic Claude 2.x and earlier models": FoundationModel("Anthropic", "Anthropic Claude 2.x and earlier models", True, True, True, True, False, False, False, True),
+    "Cohere Command": FoundationModel("Cohere", "Cohere Command", True, True, False, True, False, False, False, True),
+    "Cohere Command Light": FoundationModel("Cohere", "Cohere Command Light", True, True, False, False, False, False, False, True),
+    "Mistral AI Instruct": FoundationModel("Mistral", "Mistral AI Instruct", True, True, False, True, False, False, False, True),
+    "Mistral Large 2 (24.07)": FoundationModel("Mistral", "Mistral Large 2 (24.07)", True, True, True, True, False, True, False, True),
+    "Mistral Small": FoundationModel("Mistral", "Mistral Small", True, True, True, False, False, True, False, True),
 }
