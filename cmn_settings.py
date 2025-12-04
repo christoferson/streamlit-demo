b5583d86d1e6596e16a3f70e0dfb5ea33acf12db
christoferson <sweetsorrow1981@yahoo.co.jp>
2025-12-04 22:14:02 +0900

    Add CMN_KB_KNOWLEDGE_BASE_ID

--- cmn_settings.py
+++ cmn_settings.py
@@ -15,3 +15,19 @@ APP_LOG_GROUP_METRICS_INVOCATIONS = os.environ.get("APP_LOG_GROUP_METRICS_INVOCA
 CONVERSE_CH_SECRET_KEY = os.environ.get("CONVERSE_CH_SECRET_KEY")
 CONVERSE_CH_ALGORITHM = os.environ.get("CONVERSE_CH_ALGORITHM")
 CONVERSE_CH_SALT = os.environ.get("CONVERSE_CH_SALT")
+
+VECTOR_REGION_ID = os.getenv("VECTOR_REGION_ID", "us-east-1")
+VECTOR_EMBEDDING_MODEL_ID = os.getenv("VECTOR_EMBEDDING_MODEL_ID", "amazon.titan-embed-text-v2:0") 
+VECTOR_BUCKET_NAME = os.getenv("VECTOR_BUCKET_NAME", "s3-vector-myvectorbucket-xxx")
+VECTOR_INDEX_NAME = os.getenv("VECTOR_INDEX_NAME", "s3-vector-myvectorindex-xxxx")
+
+
+# Knowledge Base Configuration
+CMN_KB_KNOWLEDGE_BASE_ID = os.getenv("CMN_KB_KNOWLEDGE_BASE_ID", "YOUR_KB_ID_HERE")
+CMN_KB_DATA_SOURCE_ID = os.getenv("CMN_KB_DATA_SOURCE_ID", "YOUR_DATA_SOURCE_ID_HERE")
+CMN_KB_DOCUMENT_BUCKET_NAME = os.getenv("CMN_KB_DOCUMENT_BUCKET_NAME", "your-document-bucket-name")
+CMN_KB_VECTOR_BUCKET_NAME = os.getenv("CMN_KB_VECTOR_BUCKET_NAME", "your-vector-bucket-name")
+CMN_KB_VECTOR_INDEX_NAME = os.getenv("CMN_KB_VECTOR_INDEX_NAME", "your-vector-index-name")
+CMN_KB_KB_METADATA_TABLE = os.getenv("CMN_KB_KB_METADATA_TABLE", "your-kb-metadata-table")
+CMN_KB_DOC_METADATA_TABLE = os.getenv("CMN_KB_DOC_METADATA_TABLE", "your-doc-metadata-table")
+CMN_KB_MODEL_ARN = os.getenv("CMN_KB_MODEL_ARN", "arn:aws:bedrock:xxx::foundation-model/xxx")
