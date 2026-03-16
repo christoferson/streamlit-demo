f2c9a8621be4f7ba709d40fcef075ca9c45457b3
christoferson <sweetsorrow1981@yahoo.co.jp>
2026-03-16 23:04:10 +0900

    acronym

---
+++ cmn/bedrock_converse_tools_acronym.py
@@ -0,0 +1,40 @@
+from cmn.bedrock_converse_tools import AbstractBedrockConverseTool
+
+class AcronymBedrockConverseTool(AbstractBedrockConverseTool):
+
+ 
+    def __init__(self):
+        name = "acronym_evaluator"
+        definition = {
+                "toolSpec": {
+                    "name": name,
+                    "description": """Useful for when you need to decode proprietary acronym. 
+                    This tool is only resolving meanings of acronyms that are not generally defined.
+                    Tool will return the work 'unknown' is it does not know the answer.""",
+                    "inputSchema": {
+                        "json": {
+                            "type": "object",
+                            "properties": {
+                                "expression": {
+                                    "type": "string",
+                                    "description": "Acronym. Example AUP,POIE."
+                                }
+                            },
+                            "required": [
+                                "expression"
+                            ]
+                        }
+                    }
+                }
+            }
+        super().__init__(name, definition)
+
+    def invoke(self, params, tool_args=None):
+        if params == "AUP":
+            return "Access Utilization Procedure"
+        elif params == "POIE":
+            return "Procedural Oversight and Inspection Evaluation"
+        elif params == "SPT":
+            return "Sustainable Production Technologies"
+        return "unknown"
+
